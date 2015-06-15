cf6ec638a936d8922b92fbcbe175d04ec4e41567
Prisyazhnyuk Alexander <prisyagnyuk@gmail.com>
2015-06-16 00:12:45 +0300

    Add abstractBonus class

--- js/bonus.js
+++ js/bonus.js
@@ -1,39 +1,48 @@
-function Bonus(pos) {
+function AbstractBonus(pos) {
     this.pos = pos;
     this.radius = 10;
     this.addedAt = Date.now();
     this.applied = false;
+    this.color = "#fff";
+
+    this.draw = function (context) {
+        var x = this.pos.x + this.radius,
+            y = this.pos.y + this.radius;
+
+        context.beginPath();
+        context.arc(x, y, this.radius, 0, Math.PI*2, true);
+        context.fillStyle = this.color;
+        context.fill();
+        context.closePath();
+        context.fill();
+    };
+
+    this.isOutdated = function () {
+        if (Date.now() - this.addedAt > 10000) {
+            return true;
+        }
+
+        return false;
+    };
+
+    this.isApplied = function() {
+        return this.applied;
+    };
+
+    this.applyTo = function(){};
+    this.disable = function(){};
 }
 
-Bonus.prototype.draw = function (context) {
-    var x = this.pos.x + this.radius,
-        y = this.pos.y + this.radius;
+function Bonus(pos) {
+    AbstractBonus.apply(this, arguments);
+    this.color = "#33bb66";
 
-    context.beginPath();
-    context.arc(x, y, this.radius, 0, Math.PI*2, true);
-    context.fillStyle = "#33bb66";
-    context.fill();
-    context.closePath();
-    context.fill();
-};
+    this.applyTo = function (player) {
+        player.weapon.timeBetweenFire = 40;
+        this.applied = true;
+    };
 
-Bonus.prototype.isOutdated = function () {
-    if (Date.now() - this.addedAt > 10000) {
-        return true;
-    }
-
-    return false;
-};
-
-Bonus.prototype.applyTo = function (player) {
-    player.weapon.timeBetweenFire = 40;
-    this.applied = true;
-};
-
-Bonus.prototype.disable = function() {
-    Game.world.player.weapon.timeBetweenFire = 100;
-};
-
-Bonus.prototype.isApplied = function() {
-    return this.applied;
-};
+    this.disable = function() {
+        Game.world.player.weapon.timeBetweenFire = 100;
+    };
+}
